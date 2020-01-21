755a34af84d069f838454eec8004390196765b1a
sirtyilas <sirtyilas@yahoo.com>
2020-01-21 14:23:07 +0200

    auto config

--- src/main/java/com/example/companydetails/CompanyDetailsApplication.java
+++ src/main/java/com/example/companydetails/CompanyDetailsApplication.java
@@ -2,10 +2,12 @@ package com.example.companydetails;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @EnableJpaRepositories()//TODO add basepackages
 public class CompanyDetailsApplication {
 
